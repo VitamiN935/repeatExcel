56416141865f87842f1fcd82b10a298195ab10e5
VitamiN935 <maks.s923@mail.ru>
2020-05-20 15:16:59 +0500

    f

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,11 +3,41 @@ import {createTable} from '@/components/Table/table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
+
   constructor($root) {
     super($root, {
       name: 'Table',
+      listeners: ['mousedown'],
     });
   }
+
+  onMousedown(event) {
+    const target = event.target;
+    if (target.closest('.row-resize') || target.closest('.col-resize')) {
+      const resize = target.closest('.row-resize') ? event.pageY : event.pageX;
+      event.target.setAttribute('data-resize', resize);
+      event.target.classList.add('resize')
+      this.addListener('mousemove');
+      this.addListener('mouseup');
+    }
+  }
+
+  onMousemove(event) {
+    if (event.target.closest('row-resize')) {
+      let target = document.querySelector('.resize');
+
+
+    } else {
+
+    }
+
+  }
+
+  onMouseup(event) {
+    this.removeListener('mousemove')
+    this.removeListener('mouseup')
+  }
+
   toHTML() {
     return createTable()
   }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -14,6 +14,7 @@ export class ExcelComponent extends DomListener {
     this.removeDomListeners();
   }
 
+
   toHTML() {
     return ''
   }
